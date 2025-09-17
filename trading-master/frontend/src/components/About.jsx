0f077ae7d46dbe4f974870c332e8232c1c9b7cda
jkpthunderbolts <priyadarshijaykumar@gmail.com>
2025-09-17 18:50:33 +0530

    Add files via upload

---
+++ trading-master/frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+            <h1 className='text-center text-4xl font-bold text-blue-500'>About Page</h1>
+        </>
+    );
+};
+
+export default About;
